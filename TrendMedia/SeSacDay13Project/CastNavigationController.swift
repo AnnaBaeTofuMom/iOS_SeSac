410d91746fad19441a641cb528fc64bba6031013
AnnaBaeTofuMom <61861862+AnnaBaeTofuMom@users.noreply.github.com>
2021-11-15 16:42:16 +0900

    TrendMedia Added

---
+++ TrendMedia/SeSacDay13Project/CastNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  CastNavigationController.swift
+//  SeSacDay13Project
+//
+//  Created by 경원이 on 2021/10/21.
+//
+
+import UIKit
+
+class CastNavigationController: UINavigationController {
+
+    @IBOutlet var castTitle: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TrendMedia/SeSacDay13Project/MovieModel.swift
@@ -0,0 +1,17 @@
+//
+//  MovieModel.swift
+//  SeSacDay13Project
+//
+//  Created by 경원이 on 2021/10/28.
+//
+
+import Foundation
+import UIKit
+
+struct MovieModel {
+    var titleData: String
+    var imageData: String
+    var linkData: String
+    var userRatingData: String
+    var subtitle: String
+}
