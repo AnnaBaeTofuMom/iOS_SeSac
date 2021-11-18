ca4f6282105b46f31cccb655e6bb59ec3ceed202
AnnaBaeTofuMom <mymininic@gmail.com>
2021-11-18 23:03:27 +0900

    record list updated

---
+++ Projects/Rife - Simple Run Life/Rife - Simple Run Life/Storyboard/RecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordTableViewCell.swift
+//  Rife - Simple Run Life
+//
+//  Created by 경원이 on 2021/11/18.
+//
+
+import UIKit
+
+class RecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
